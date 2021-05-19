f3d7539349b71fc499bb5b3f3f29e0fa040519fa
Carl <carlbrainegebe@gmail.com>
2021-05-19 16:09:46 -0500

    commit tree af2b55c7e0da7f8a170ba089802a77a89a168ff7

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Your code here
+
+function saturdayFun(activity = "roller-skate") {
+const plans = `This Saturday, I want to ${activity}!`;
+return plans;
+}
+
+const mondayWork = function(chore = "go to the office") {
+    const plans = `This Monday, I will ${chore}.`;
+
+    return plans;
+}
+
+
+ function wrapAdjective(flair = "*") {
+    
+    return function(description = "special") {
+        const message = `You are ${flair}${description}${flair}!`;
+        return message;
+    }
+}
+
+//wrapAdjective($)("an idiot");
+
+// function outer(greeting, msg) {
+
+//     return function(name, lang) {
+//        return `${greeting}, ${name}! ${msg} ${lang}`;
+//     }
+// return innerFunction;
+// }
+
+// outer("Hello", "Its a fine day to learn")("student", "Javascript");
+
+const array = (
+    function(thingToAdd) {
+      const base = 3;
+      return [
+        function() { return base + thingToAdd; },
+        function() { return base; }
+      ];
+    }
+  )(2)
